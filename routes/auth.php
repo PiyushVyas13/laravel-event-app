66c91b508ca30f42071a608fb52e0e912166490c
priyachau12 <125198162+priyachau12@users.noreply.github.com>
2023-12-19 15:12:49 +0530

    Update auth.php
    updated

--- routes/auth.php
+++ routes/auth.php
@@ -53,6 +53,7 @@ Route::middleware('auth')->group(function () {
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
